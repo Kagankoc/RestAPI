465954664223bedee3747f3ed863d9d640429300
Kağan Koç <cemsukagan@hotmail.com>
2020-02-24 10:45:47 +0300

    Add project files.

---
+++ RestAPI/Controllers/PagesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PagesController : ControllerBase
+    {
+        private readonly EShopContext _context;
+
+        public PagesController(EShopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Page>>> GetPages()
+        {
+            return await _context.Pages.OrderBy(x => x.Sorting).ToListAsync();
+        }
+    }
+}
---
+++ RestAPI/Models/EShopContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestAPI.Models
+{
+    public class EShopContext : DbContext
+    {
+        public EShopContext(DbContextOptions<EShopContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Page> Pages { get; set; }
+
+    }
+}
